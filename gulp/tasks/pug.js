dc7ee72a4ad0da7dead6b481838a22cf0c827561
iibadreeva <iibadreeva@mail.ru>
2018-07-08 16:45:12 +0300

    pug add include

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,7 +2,7 @@
 
 module.exports = function () {
     $.gulp.task('pug', function () {
-        return $.gulp.src('./app/source/template/*.pug')
+        return $.gulp.src('./app/source/template/page/*.pug')
             .pipe($.gp.pug({ pretty: true }))
             .on('error', $.gp.notify.onError(function (error) {
                 return {
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,6 +3,6 @@
 module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('./app/source/style/*.scss', $.gulp.series('sass'));
-        $.gulp.watch('./app/source/template/*.pug', $.gulp.series('pug'))
+        $.gulp.watch('./app/source/template/**/*.pug', $.gulp.series('pug'))
     });
 };
